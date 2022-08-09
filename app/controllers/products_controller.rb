4866f3850d2bbffdef0d0f582b657d5654a2e114
Maksym Hostroushko <makshostroyshko@gmail.com>
2022-08-09 16:39:27 +0300

    init project with base funcionality

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,17 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.last
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  root "products#index"
+
+  resources :products, only: %i[index show new create]
+end
