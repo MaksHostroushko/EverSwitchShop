ecba2e9a06d3bb7dcb10a58b2491a403b32ce4c9
Maksym Hostroushko <makshostroyshko@gmail.com>
2022-09-06 16:27:39 +0300

    Add logic for order creating without current user

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -5,11 +5,12 @@ class OrderItemsController < ApplicationController
     if @order_item
       @order_item.increment!(:quantity)
     else
-      @order_item = current_order.order_items.create(product_id: params[:product_id])
+      @order_item = current_order.order_items.build(product_id: params[:product_id])
+      @order_item.save
+      session[:order_id] = @order_item.order_id unless session[:order_id]
     end
 
-    redirect_to root_path
-    flash.alert = "#{@order_item.product.title} added to cart."
+    redirect_to root_path, notice: "#{@order_item.product.title} added to cart."
   end
 
   def update
