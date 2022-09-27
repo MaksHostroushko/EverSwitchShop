b55533052cba50096be56e6c1251f989b502d2e9
Maksym Hostroushko <makshostroyshko@gmail.com>
2022-09-27 10:19:04 +0300

    Refactor: fix migration data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_155954) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_06_131613) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -90,7 +90,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_155954) do
     t.string "address"
     t.string "full_name"
     t.string "phone"
-    t.string "name"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
@@ -128,7 +127,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_155954) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.integer "role"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
